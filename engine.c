ad40e6826f36817961ab55fd87a03b5609069208
YukunJ <yukunj@andrew.cmu.edu>
2024-10-28 22:34:29 -0400

    Implement the price_level: a doubly linked list of orders at the same price and side

--- engine.c
+++ engine.c
@@ -3,10 +3,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 
 #include "engine.h"
 
 #define MIN(a, b) ((a < b) ? (a) : (b))
+#define MAX(a, b) ((a > b) ? (a) : (b))
 
 // Internal Implementation
 typedef struct order {
@@ -14,135 +16,92 @@ typedef struct order {
   t_order order;
 } order_t;
 
-#define TYPE order_t
-#define EMPTY_VAL (TYPE) {0, {'\0', '\0', 0, 0, 0}}
-#define PRINTER(data) printf("[order_id: %ld symbol: %s trader: %s side: %d price: %d size: %ld] ", data.id, data.order.symbol, data.order.trader, data.order.side, data.order.price, data.order.size);
-#include "single_list.c"
+typedef struct order_node {
+  order_t order_obj;
+  struct order_node *prev;
+  struct order_node *next;
+} order_node_t;
 
-// -- GLOBALS -- 
-static single_list_t *bid_list;
-static single_list_t *ask_list;
-static t_orderid id_counter;
+typedef struct price_level {
+  t_side side;
+  t_price price;
+  order_node_t *header;
+  order_node_t *tailer;
+} price_level_t;
 
-/* return if the new price is superior in priority with the old price 
-   in bid book it means new price is strictly higher than old price
-   in ask book it means new price is strictly lower than old price
-*/
-static bool is_superior(bool ask, t_price new_price, t_price old_price) {
-  return (ask) ? (new_price < old_price) : (new_price > old_price);
+price_level_t *init_price_level(t_side side, t_price price) {
+  price_level_t *pl = (price_level_t *)malloc(sizeof(price_level_t));
+  pl->side = side;
+  pl->price = price;
+  pl->header = (order_node_t *)malloc(sizeof(order_node_t));
+  pl->tailer = (order_node_t *)malloc(sizeof(order_node_t));
+  pl->header->prev = NULL;
+  pl->header->next = pl->tailer;
+  pl->tailer->prev = pl->header;
+  pl->tailer->next = NULL;
+  return pl;
 }
 
-static void report_execution(t_order buy_order, t_order sell_order, t_price trade_price, t_size trade_qty) {
-  t_execution exe;
-  exe.price = trade_price;
-  exe.size = trade_qty;
-  exe.side = 0;
-  strncpy(exe.symbol, buy_order.symbol, STRINGLEN);
-  strncpy(exe.trader, buy_order.trader, STRINGLEN);
-  execution(exe);
-  exe.side = 1;
-  strncpy(exe.symbol, sell_order.symbol, STRINGLEN);
-  strncpy(exe.trader, sell_order.trader, STRINGLEN);
-  execution(exe);
-}
-
-/* add an order to the list, assuming it will not cross the book and no trade can happen with its addition */
-static void add_order(t_order order, t_orderid id) {
-  if (order.size <= 0) { return; }
-  bool ask = is_ask(order.side);
-  order_t new_order = {id, order};
-  single_list_t *list = (ask) ? ask_list : bid_list;
-  node_t *curr = list->header;
-  while (curr->next) {
-    if (is_superior(ask, order.price, curr->next->data.order.price)) {
-      insert_after(curr, new_order);
-      return;
-    }
-    curr = curr->next;
+void delete_price_level(price_level_t *pl) {
+  order_node_t *next;
+  order_node_t *current = pl->header->next;
+  while (current != pl->tailer) {
+    next = current->next;
+    free(current);
+    current = next;
   }
-  insert_after(curr, new_order);
+  free(pl->header);
+  free(pl->tailer);
+  free(pl);
 }
 
-/* uncross the book and execute trade accordingly */
-static t_order uncross_book(t_order incoming_order) {
-  if (incoming_order.size == 0) { return incoming_order; }
-  bool ask = is_ask(incoming_order.side);
-  if (ask) {
-    node_t *bid_current = bid_list->header;
-    if (bid_current->next && bid_current->next->data.order.price >= incoming_order.price) {
-      t_order best_bid_order = bid_current->next->data.order;
-      t_price trade_price = best_bid_order.price;
-      t_size trade_size = MIN(best_bid_order.size, incoming_order.size);
-      report_execution(best_bid_order, incoming_order, trade_price, trade_size);
-      incoming_order.size -= trade_size;
-      bid_current->next->data.order.size -= trade_size;
-      if (bid_current->next->data.order.size == 0) {
-        delete_after(bid_current);
-      }
-      return uncross_book(incoming_order);
-    } else {
-      return incoming_order;
-    }
-  } else {
-    node_t *ask_current = ask_list->header;
-    if (ask_current->next && ask_current->next->data.order.price <= incoming_order.price) {
-      t_order best_ask_order = ask_current->next->data.order;
-      t_price trade_price = best_ask_order.price;
-      t_size trade_size = MIN(best_ask_order.size, incoming_order.size);
-      report_execution(incoming_order, best_ask_order, trade_price, trade_size);
-      incoming_order.size -= trade_size;
-      ask_current->next->data.order.size -= trade_size;
-      if (ask_current->next->data.order.size == 0) {
-        delete_after(ask_current);
-      }
-      return uncross_book(incoming_order);
-    } else {
-      return incoming_order;
-    }
+void insert_order_at_tail(price_level_t *pl, t_orderid id, t_order order) {
+  assert(order.price == pl->price && order.side == pl->side); // order in this price_level should be of this price and side
+  order_node_t *new_node = (order_node_t *)malloc(sizeof(order_node_t));
+  new_node->order_obj.id = id;
+  new_node->order_obj.order = order;
+  new_node->next = pl->tailer;
+  new_node->prev = pl->tailer->prev;
+  pl->tailer->prev->next = new_node;
+  pl->tailer->prev = new_node;
+}
+
+void remove_order_node(order_node_t *node) {
+  // should never delete the header and tailer node of a price_level
+  assert(node->prev != NULL);
+  assert(node->next != NULL);
+  node->prev->next = node->next;
+  node->next->prev = node->prev;
+  free(node);
+}
+
+void print_price_level(price_level_t *pl) {
+  printf("price level %s at %d\n", (is_ask(pl->side)) ? "ASK" : "BID", pl->price);
+  order_node_t *current = pl->header->next;
+  while (current != pl->tailer) {
+    printf("Order %lu by trader %s on symbol %s size %lu\n", current->order_obj.id, current->order_obj.order.trader, current->order_obj.order.symbol, current->order_obj.order.size);
+    current = current->next;
   }
+  printf("\n");
 }
 
-/* try to delete an order with id specified from a list. return True if successfully find and delete it. */
-static bool try_delete_order(single_list_t *list, t_orderid id) {
-  for (node_t *node = list->header; node != NULL; node = node->next) {
-    if (node->next && node->next->data.id == id) {
-      delete_after(node);
-      return true;
-    }
-  }
-  return false;
-}
+// int main(void) {
 
-// Interface Implementation
-void init() {
-  bid_list = init_single_list();
-  ask_list = init_single_list();
-  id_counter = 1;
-}
+//   price_level_t *pl = init_price_level((t_side)0, (t_price)100);
+//   t_order order = {"BMW", "JYK", 0, 100, 200};
+//   insert_order_at_tail(pl, 1, order);
+//   order.size = 50;
+//   insert_order_at_tail(pl, 2, order);
+//   order.size = 100;
+//   insert_order_at_tail(pl, 3, order);
+//   order.size = 150;
+//   insert_order_at_tail(pl, 4, order);
+//   print_price_level(pl);
+//   order_node_t *to_remove = pl->header->next->next;
+//   to_remove->order_obj.order.size = 0;
+//   remove_order_node(to_remove);
+//   print_price_level(pl);
+//   return 0;
+// }
 
-/* IN:
-   OUT: */
-void destroy() {
-  destroy_single_list(bid_list);
-  destroy_single_list(ask_list);
-}
 
-/* IN: order: limit order to add to book
-   OUT: orderid assigned to order 
-        start from 1 and increment with each call */
-t_orderid limit(t_order order) {
-  t_orderid new_id = id_counter++;
-  t_order remaining_order = uncross_book(order);
-  add_order(remaining_order, new_id);
-  return new_id;
-}
-
-/* IN: orderid: id of order to cancel
-   OUT:
-   cancel request ignored if orderid not in book
-*/
-void cancel(t_orderid orderid) {
-  if (!try_delete_order(bid_list, orderid)) {
-    try_delete_order(ask_list, orderid);
-  }
-}
