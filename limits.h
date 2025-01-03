9e82525e468f918bb8b1b56f646b982d4d753625
YukunJ <yukunj@andrew.cmu.edu>
2025-01-03 10:48:13 +0800

    Standard implementation

--- limits.h
+++ limits.h
@@ -1,7 +1,7 @@
 /* Maximum price of a limit order 
    655.36 dollars 
    max size of unsigned short */
-#define MAX_PRICE 65536
+#define MAX_PRICE 65535
 
 /* Minimum price of a limit order 
    0.01 dollars */
@@ -10,7 +10,7 @@
 /* Maximum number of uncrossed orders that
    may be sitting on the book at a time.
    Gives the implementor a finite bound */
-#define MAX_LIVE_ORDERS 65536
+#define MAX_LIVE_ORDERS 65535
 
 /* Maximum number of characters in both
    symbol and trader fields in order */
