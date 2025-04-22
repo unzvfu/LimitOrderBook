ade6782042bb37e1efdc8771565c19af20b75d91
Hamish Ivey-Law <hamish@ivey-law.name>
2025-04-22 16:41:13 +0400

    Use winning engine; fix latency calculations; calculate iteratively; take params as arguments.

--- score.c
+++ score.c
@@ -7,7 +7,8 @@
 #endif
 #include "limits.h"
 #include "types.h"
-#include "engine.c"
+#include "winning_engine.c"
+//#include "engine.c"
 #include "score_feed.h"
 
 void feed(unsigned begin, unsigned end);
@@ -38,54 +39,62 @@ void current_utc_time(struct timespec *ts) {
 }
 
 
-int msg_batch_size = 200;
-int replays = 2000;
 void execution(t_execution exec) {};
 
-int main() {
-    print_cpuaffinity();
+int main(int argc, char *argv[]) {
+    int msg_batch_size = 200;
+    int replays = 2000;
+
+    if (argc > 1) {
+        msg_batch_size = atoi(argv[1]);
+    }
+    if (argc > 2) {
+        replays = atoi(argv[2]);
+    }
     int raw_feed_len = sizeof(raw_feed)/sizeof(t_order);
-    int samples = replays * (raw_feed_len/msg_batch_size);
-    long long late[samples]; // batch latency measurements
-  
- 
+    int nbatches = raw_feed_len/msg_batch_size;
+    int samples = replays * nbatches;
+
+    printf("batches: %d\n", nbatches);
+    printf("batch size: %d\n", msg_batch_size);
+    printf("replays: %d\n", replays);
+    printf("raw feed length: %d\n", raw_feed_len);
+    printf("samples: %d\n", samples);
+
+    long long sum = 0;
+    long long squares = 0;
+
     struct timespec begin;
     struct timespec end;
 
-    int j; for (j = 0; j < replays; j++) {
+    for (int j = 0; j < replays; j++) {
         init();
-        int i; for (i = msg_batch_size; i < raw_feed_len; i += msg_batch_size) {
+        for (int i = msg_batch_size; i < raw_feed_len; i += msg_batch_size) {
             current_utc_time(&begin);
 
             feed(i-msg_batch_size, i);
 
             current_utc_time(&end);
-            late[i/msg_batch_size - 1 + (j*(raw_feed_len/msg_batch_size))] = timediff(begin, end);
+
+            long long dt = timediff(begin, end);
+            sum += dt;
+            squares += dt*dt;
         }
         destroy();
     }
 
-    long long late_total = 0LL;
-    int i; for (i = 0; i < samples; i++) { late_total += late[i]; }
-    double late_mean = ((double)late_total) / ((double)samples);
-    double late_centered = 0;
-    double long late_sqtotal = 0LL;
-    for (i = 0; i < samples; i++) {
-        late_centered = ((double)late[i]) - late_mean;
-        late_sqtotal += late_centered*late_centered/((double)samples);
-    }
-    double late_sd = sqrt(late_sqtotal);
-    printf("mean(latency) = %1.2f, sd(latency) = %1.2f\n", late_mean, late_sd);
-
-    double score = 0.5 * (late_mean + late_sd);
-    printf("You scored %1.2f. Try to minimize this.\n", score);
-    printf("%1.2f\n", late_mean);
-    printf("%1.2f\n", late_sd);
-    printf("%1.2f", score);
+    double latency_mean = sum / (double)samples; // mean latency per batch
+    double latency_sd = sqrt(squares / (double)samples - latency_mean * latency_mean); // latency stddev per batch
+    double score = 0.5 * (latency_mean + latency_sd) * nbatches;
+    printf("Mean:     %5.1f μs\n", 1e-3 * latency_mean * nbatches);
+    printf("Stddev:   %5.1f μs\n", 1e-3 * latency_sd * nbatches); // σ
+    printf("QC score: %ld\n", (long long)score);
+    printf("avg/tx:   %.2f ns\n", latency_mean / (double)msg_batch_size);
+    //printf("avg/tx:   %.2f ns\n", latency_mean * nbatches / (double) raw_feed_len);
 }
 
 void feed(unsigned begin, unsigned end) {
-    int i; for(i = begin; i < end; i++) {
+    for(int i = begin; i < end; i++) {
         if (raw_feed[i].price == 0) {
             cancel(raw_feed[i].size);
         } else {
@@ -106,14 +115,22 @@ long long timediff(struct timespec start, struct timespec end) {
     return (temp.tv_sec*1000000000) + temp.tv_nsec;
 }
 
-void print_cpuaffinity() {
-#ifndef __MACH__
-    unsigned long mask = 2; /* processor 1 (0-indexed) */
-    unsigned int len = sizeof(mask);
-    if (sched_getaffinity(0, len, &mask) < 0) {
-        perror("sched_getaffinity");
-    }
-    printf("my affinity mask is: %08lx\n", mask);
-#endif
-}
 
+// #define _GNU_SOURCE
+// #include <sched.h>
+//
+// void print_cpuaffinity() {
+// #ifndef __MACH__
+//     cpu_set_t mask; /* processor 1 (0-indexed) */
+//     unsigned int len = sizeof(mask);
+//
+//     if (sched_getaffinity(0, len, &mask) < 0) {
+//         perror("sched_getaffinity");
+//     }
+//     printf("active cores: ");
+//     for (int i = 0; i < 32; ++i)
+//         if (CPU_ISSET(i, &mask))
+//             printf("%d ", i);
+//     printf("\n");
+// #endif
+// }
